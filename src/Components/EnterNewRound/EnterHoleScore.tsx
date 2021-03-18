2dc8d56f32c905247d0cf5b66fa315cb280200ca
JoksuH <haarlaj@gmail.com>
2021-03-18 15:16:54 +0200

    Added basic outline components

---
+++ src/Components/EnterNewRound/EnterHoleScore.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+          
+        </div>
+    )
+}
+
+export default App
+
+/*
+- score
+- putts
+- fir   => checkbox or miss where? left right
+- gir	=> checkbox or miss where? long short, left right
+- sand shots => number (can be empty to mean 0)
+- 1st putt distance on gir  => ask this if gir checked
+- Ob / penalties
+*/
